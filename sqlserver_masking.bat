1bd7dedae3bf4895cca60381c6bd6626cc3fd20b
Makoto123J <makotoonodera3@gmail.com>
2024-08-14 04:01:06 +0900

    Add files via upload

--- sqlserver_masking.bat
+++ sqlserver_masking.bat
@@ -5,3 +5,12 @@ sqlcmd -S MSSQLSERVER -d SQLServer_Demo -Q "UPDATE [dbo].[DimCustomer] SET [Firs
 
 REM スクリプトを終了
 EXIT /B
+
+
+
+プリスクリプト
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Makoto123J/masking/main/sqlserver_masking.bat" -OutFile "C:\Temp\sqlserver_masking.bat"
+
+
+ポストスクリプト
+sqlserver_masking.bat
