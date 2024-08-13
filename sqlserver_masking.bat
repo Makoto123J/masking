71f732f4305728c72d3bbeec30e0f5b5c69815ad
Makoto123J <makotoonodera3@gmail.com>
2024-08-14 04:02:55 +0900

    Update sqlserver_masking.bat

--- sqlserver_masking.bat
+++ sqlserver_masking.bat
@@ -5,12 +5,3 @@ sqlcmd -S MSSQLSERVER -d SQLServer_Demo -Q "UPDATE [dbo].[DimCustomer] SET [Firs
 
 REM スクリプトを終了
 EXIT /B
-
-
-
-プリスクリプト
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Makoto123J/masking/main/sqlserver_masking.bat" -OutFile "C:\Temp\sqlserver_masking.bat"
-
-
-ポストスクリプト
-sqlserver_masking.bat
