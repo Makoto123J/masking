c30da7458c0fcc59ed69465fb7949e78e2e4afbf
Makoto123J <makotoonodera3@gmail.com>
2024-08-14 13:24:31 +0900

    Add files via upload

---
+++ Update.py
@@ -0,0 +1,28 @@
+﻿# -*- coding: utf-8 -*-
+
+
+import pyodbc
+
+# 接続情報を設定
+conn_str = (
+    r'DRIVER={ODBC Driver 17 for SQL Server};'
+    r'SERVER=WIN-MN58QKLPKCS;'
+    r'DATABASE=SQLServer_Demo;'
+    r'Trusted_Connection=yes;'
+)
+
+# 接続を開く
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
+
+# クエリを実行
+cursor.execute("UPDATE [dbo].[DimCustomer] SET [FirstName] = '*****', [MiddleName] = '*****', [LastName] = '*****', [EmailAddress] = '*****'")
+
+# 変更をコミット
+conn.commit()
+
+# クローズ
+cursor.close()
+conn.close()
+
+print("クエリが正常に実行されました。")
